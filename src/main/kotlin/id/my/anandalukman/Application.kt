33e5d04b3be10890a5c143dc69a300fe33358e4c
Ananda Lukman <ananda.muhamad.lukman@gmail.com>
2022-02-12 14:27:02 +0700

    rollback

--- src/main/kotlin/id/my/anandalukman/Application.kt
+++ src/main/kotlin/id/my/anandalukman/Application.kt
@@ -2,17 +2,12 @@ package id.my.anandalukman
 
 import io.ktor.application.*
 import id.my.anandalukman.plugins.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
-        // ...
-    }.start(wait = true)
     configureRouting()
     configureSerialization()
     configureMonitoring()
